1b931dffc64c16a24f41dedbdaad89c2b76c9124
DilnLemos <dilan.lemos@correounivalle.edu.co>
2026-03-10 15:43:17 -0500

    Feat, Fix: Conexión DB, arreglo sql
    Ya se hizo conexión a la base de datos en SupaBase y se corrigió error en el script de las tablas

--- src/database.sql
+++ src/database.sql
@@ -6,7 +6,7 @@ CREATE TABLE dim_customer (
 
 CREATE TABLE dim_category (
     id_category INTEGER PRIMARY KEY,
-    category TEXT,
+    category TEXT
 );
 
 CREATE TABLE dim_date (
@@ -31,10 +31,10 @@ CREATE TABLE fact_sales (
     id_invoice INTEGER PRIMARY KEY,
     id_customer INTEGER REFERENCES dim_customer(id_customer),
     id_category INTEGER REFERENCES dim_category(id_category),
-    id_date DATE REFERENCES dim_date(id_date),
+    id_date INTEGER REFERENCES dim_date(id_date),
     id_mall INTEGER REFERENCES dim_store(id_mall),
     id_payment_method INTEGER REFERENCES dim_payment(id_payment_method),
     quantity INTEGER,
-    unit_price INTEGER,
-    total_price INTEGER,
+    unit_price NUMERIC(10,2),
+    total_price NUMERIC(10,2)
 );
